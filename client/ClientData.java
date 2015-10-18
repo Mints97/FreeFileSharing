9ba60109260cd49ab56d66697dcf7edc59877c33
Maxim Mints <max.mints@mail.ru>
2015-10-17 22:56:24 -0400

    Updated ClientData method headers

--- client/ClientData.java
+++ client/ClientData.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.net.Socket;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 
 /**
  *  Represents a client. Is a singleton
@@ -8,7 +12,18 @@ public enum ClientData {
     
     public final static int NUM_SERVERS_PER_PAGE = 10;
     
+    
+    public final static String MAIN_SERVER_IP = "128.61.119.119";
+    public final static int MAIN_SERVER_PORT = 300;
+    
+    
+    private int pageNumber = 0;
+    
     private ServerData[] servers = new ServerData[NUM_SERVERS_PER_PAGE];
+    
+    private ClientData() {
+        /* ... */
+    }
     
     public ServerData getServer(int i) {
         return this.servers[i];
@@ -18,15 +33,35 @@ public enum ClientData {
         return INSTANCE;
     }
     
-    private ClientData() {
-        /* ... */
+    public static String sendDataToMainServer(String data) {
+        try {
+            Socket client = new Socket(MAIN_SERVER_IP, MAIN_SERVER_PORT);
+            
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            out.writeUTF(data);
+            
+            DataInputStream in = new DataInputStream(client.getInputStream());
+            String result = in.readUTF();
+            
+            client.close();
+            
+            return result;
+        } catch(IOException e) {
+            return "error";
+        }
     }
     
     public static void startUpdateThread() {
         /* ... */
     }
     
-    public static void uploadFile(File file, ServerData... servers) {
+    public static void uploadFile(File file, ServerData server) {
+        /* ... */
+    }
+    
+    public static void uploadFile(File file,
+                                  String key,
+                                  ServerData server) {
         /* ... */
     }
     
@@ -34,7 +69,9 @@ public enum ClientData {
         /* ... */
     }
     
-    public File downloadFile(int fileID, ServerData... servers) {
+    public File downloadFile(int fileID,
+                             String key,
+                             ServerData server) {
         /* ... */
         return null;
     }
